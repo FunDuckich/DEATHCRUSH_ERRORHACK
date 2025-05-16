6c239fda02d0610bec284ec86ed0ff4c3f78aa5c
FunDuckich <petrovegor495@gmail.com>
2025-05-16 14:32:35 +0400

    начало фронта

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import TestButton from './components/TestButton.jsx'
+import FileUploadPanel from './components/FileUploadPanel'
 
 function App() {
-
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <TestButton/>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+            <FileUploadPanel/>
         </div>
     )
 }
