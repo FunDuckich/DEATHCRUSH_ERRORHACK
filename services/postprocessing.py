8ca219adcb3e40c2669bc3cbf9acb2dba13988fb
FunDuckich <petrovegor495@gmail.com>
2025-05-17 18:41:51 +0400

    улучшил предобработку и сделал кластеризаци на бэке

--- services/postprocessing.py
+++ services/postprocessing.py
@@ -5,10 +5,14 @@ from typing import List, Dict, Any
 def group_by_cluster(df: pd.DataFrame) -> List[Dict[str, Any]]:
     grouped: List[Dict[str, Any]] = []
 
-    for cluster_id, group in df.groupby("cluster"):
-        files = group[["filename", "hash", "epoch", "version", "release", "arch", "updated", "ftbfs_since", "url", "error", ]].to_dict(orient="records")
+    for cluster_name, group in df.groupby("cluster"):
+        files = group[[
+            "filename", "hash", "epoch", "version", "release", "arch",
+            "updated", "ftbfs_since", "url", "error"
+        ]].to_dict(orient="records")
+
         grouped.append({
-            "cluster": "",
+            "cluster": cluster_name,
             "files": files
         })
 
